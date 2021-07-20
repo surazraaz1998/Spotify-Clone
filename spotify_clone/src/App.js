7b4a75369faf9812b5a11a706d04bc99c7e1b8b5
Suraj Raj <srraaz1998@gmail.com>
2021-07-21 00:21:49 +0530

    Day One of coding completed

--- spotify_clone/src/App.js
+++ spotify_clone/src/App.js
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-
+import Login from "./Login"
+import { getTokenFromResponseUrl } from './spotify';
 function App() {
+  const [token, settoken] = useState(null);
+  useEffect(() => {
+    const hash =getTokenFromResponseUrl();
+    window.location.hash="";
+    const _token=hash.access_token;
+    if(_token){
+      settoken(_token)
+    }
+    
+  }, [])
   return (
-    <div className="App">
-    <h1>I will build a Spotify Clone</h1>
-    <p>You are amazing just keep going</p>
+    <div className="app">
+      {
+        token ? (<h1>I am logged in</h1>):(<Login/>)
+      }
     </div>
   );
 }
